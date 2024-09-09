fc6bf10fd35c22d72ab168c4a50921fdd24c825e
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-08 20:00:47 -0600

    Add files via upload

---
+++ operaciones.py
@@ -0,0 +1,23 @@
+a = 10
+b = 3
+
+# Suma
+suma = a + b
+
+# Resta
+resta = a - b
+
+# Multiplicación
+multiplicacion = a * b
+
+# División
+division = a / b  # División decimal
+division_entera = a // b  # División entera
+
+# Potencia
+potencia = a ** b
+
+# Módulo (resto de la división)
+modulo = a % b
+
+print(suma, resta, multiplicacion, division, division_entera, potencia, modulo)
