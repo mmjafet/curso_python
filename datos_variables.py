600b26b0e3f73606f28189e0b38820e4bfc9d5c8
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-08 20:00:59 -0600

    Add files via upload

---
+++ datos_variables.py
@@ -0,0 +1,14 @@
+# Entero
+edad = 25
+
+# Decimal
+altura = 1.75
+
+# Cadena
+nombre = "Ana"
+
+# Booleano
+es_estudiante = True
+
+# Imprime las variables
+print(edad, ":",":", altura,":", nombre,":", es_estudiante)
