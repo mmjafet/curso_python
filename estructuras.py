7fef199b984cba41c8c4cdac23db660da2d7c11e
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-12 23:44:45 -0600

    Add files via upload

---
+++ estructuras.py
@@ -0,0 +1,9 @@
+# Programa que evalúa si un número es positivo, negativo o cero
+numero = int(input("Introduce un número: "))
+
+if numero > 0:
+    print("El número es positivo.")
+elif numero < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")
