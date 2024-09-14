fc0a2bfbc863be2e8adb35317b3eaa160579cd6b
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-13 23:24:21 -0600

    Add files via upload

---
+++ poo.py
@@ -0,0 +1,14 @@
+# Definición de una clase Persona
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        print(f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años.")
+
+# Crear un objeto de la clase Persona
+persona1 = Persona("Jafet", 30)
+
+# Llamar al método saludar
+persona1.saludar()
