d7d8a4bd6ff1e2d29d6e8ac821b4d303ad8cc66b
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-13 23:22:44 -0600

    Add files via upload

---
+++ funcion_retorno.py
@@ -0,0 +1,7 @@
+# Función que suma dos números y devuelve el resultado
+def sumar(a, b):
+    return a + b
+
+# Llamada a la función y guardando el resultado en una variable
+resultado = sumar(5, 3)
+print("La suma es:", resultado)
