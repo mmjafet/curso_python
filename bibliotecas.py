68b196f04823baa0ca60e19e2712a862228772d7
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-13 23:23:36 -0600

    Add files via upload

---
+++ bibliotecas.py
@@ -0,0 +1,8 @@
+# Usar la biblioteca math para cálculos matemáticos
+import math
+
+# Calcular la raíz cuadrada de un número
+numero = 16
+raiz_cuadrada = math.sqrt(numero)
+
+print(f"La raíz cuadrada de {numero} es {raiz_cuadrada}")
