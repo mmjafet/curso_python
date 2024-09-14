9b63e8604fd381aa74437a24d7f1367f46c12339
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-14 10:28:54 -0600

    Add files via upload

---
+++ Numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Crear un arreglo de números
+arreglo = np.array([1, 2, 3, 4, 5])
+
+# Calcular la suma de los elementos del arreglo
+suma = np.sum(arreglo)
+
+print("Suma del arreglo:", suma)
