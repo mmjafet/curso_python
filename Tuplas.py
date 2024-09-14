4d289fdc6ed7a3ccc92bd1095abdd1c409b88b86
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-14 10:29:32 -0600

    Add files via upload

---
+++ Tuplas.py
@@ -0,0 +1,7 @@
+# Definición de una tupla
+coordenadas = (10, 20)
+
+# Acceder a elementos
+print(coordenadas[0])
+
+# Las tuplas no pueden ser modificadas, pero pueden ser utilizadas para almacenar datos constantes.
