72448ef4a6dc60e4ad292437af2e53f0816c310e
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-14 10:29:21 -0600

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,15 @@
+# Definición de una lista
+numeros = [1, 2, 3, 4, 5]
+
+# Acceder a elementos
+print(numeros[0])  # Primer elemento
+
+# Añadir elementos
+numeros.append(6)
+
+# Eliminar elementos
+numeros.remove(3)
+
+# Recorrer la lista
+for numero in numeros:
+    print(numero)
