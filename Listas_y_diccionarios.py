faa4b93fdf288f89d4a778c56c14809f3732ac1c
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-10-03 22:05:57 -0600

    Add files via upload

---
+++ Listas_y_diccionarios.py
@@ -0,0 +1,19 @@
+# Lista de listas
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Acceder a un elemento de la matriz
+print(matriz[1][2])  # Resultado: 6
+
+# Diccionario de diccionarios
+empleados = {
+    '001': {'nombre': 'Ana', 'puesto': 'Gerente'},
+    '002': {'nombre': 'Luis', 'puesto': 'Desarrollador'},
+    '003': {'nombre': 'Pedro', 'puesto': 'Analista'}
+}
+
+# Acceder a los detalles de un empleado
+print(empleados['002']['nombre'])  # Resultado: Luis
