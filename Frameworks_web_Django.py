cbf3088d9dd3a2531b1881911146f91f259adbde
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-10-21 23:05:19 -0600

    Create Frameworks_web_Django.py

---
+++ Frameworks_web_Django.py
@@ -0,0 +1,11 @@
+# Instalar Django
+pip install django
+
+# Crear un nuevo proyecto
+django-admin startproject mi_proyecto
+
+# Navegar a la carpeta del proyecto
+cd mi_proyecto
+
+# Iniciar el servidor de desarrollo
+python manage.py runserver
